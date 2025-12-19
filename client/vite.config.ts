9ce1050b4aff0b42890bbf5213d76b9cb5983a84
Hans Yadav <hansy@users.noreply.github.com>
2025-12-18 23:25:45 -0500

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,9 +3,13 @@ import { defineConfig } from "vite";
 export default defineConfig({
   root: __dirname,
   server: {
-    port: 5173
+    port: 5173,
+    allowedHosts: [
+      "localhost",
+      "unmeanderingly-nontheosophic-alex.ngrok-free.dev",
+    ],
   },
   build: {
-    outDir: "dist"
-  }
+    outDir: "dist",
+  },
 });
